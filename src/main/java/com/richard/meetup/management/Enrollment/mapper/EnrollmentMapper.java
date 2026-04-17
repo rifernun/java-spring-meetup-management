2e31b5434d71cae8829d4b63b8760d6d24f04540
Rifernun <rifernun@gmail.com>
2026-04-17 13:31:46 -0300

    feat(mapper): add static methods for entity conversion in Enrollment and Participant mappers

--- src/main/java/com/richard/meetup/management/Enrollment/mapper/EnrollmentMapper.java
+++ src/main/java/com/richard/meetup/management/Enrollment/mapper/EnrollmentMapper.java
@@ -1,13 +1,24 @@
 package com.richard.meetup.management.Enrollment.mapper;
 
+import com.richard.meetup.management.Enrollment.dto.EnrollmentRequestDto;
 import com.richard.meetup.management.Enrollment.dto.EnrollmentResponseDto;
 import com.richard.meetup.management.Enrollment.entity.Enrollment;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
 public class EnrollmentMapper {
 
-    public EnrollmentResponseDto toResponseDto(Enrollment enrollment) {
+    public static Enrollment toEntity(EnrollmentRequestDto enrollmentRequestDto) {
+        if (enrollmentRequestDto == null) return null;
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.setDateTime(Instant.now());
+        return enrollment;
+    }
+
+    public static EnrollmentResponseDto toResponseDto(Enrollment enrollment) {
         if (enrollment == null) return null;
 
         return new EnrollmentResponseDto(
