c9d25b60ee6546e88b20173d1c99e6f72117ab2f
Rifernun <rifernun@gmail.com>
2026-04-19 14:55:27 -0300

    feat(utils): update CountRemainingSpots to consider only active enrollments

--- src/main/java/com/richard/meetup/management/shared/utils/CountRemainingSpots.java
+++ src/main/java/com/richard/meetup/management/shared/utils/CountRemainingSpots.java
@@ -1,12 +1,15 @@
 package com.richard.meetup.management.shared.utils;
 
+import com.richard.meetup.management.Enrollment.entity.enums.EnrollmentStatus;
 import com.richard.meetup.management.Enrollment.repository.EnrollmentRepository;
 import com.richard.meetup.management.Event.entity.Event;
+import com.richard.meetup.management.Event.repository.EventRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Component
 @AllArgsConstructor
@@ -15,7 +18,12 @@ public class CountRemainingSpots {
     private final EnrollmentRepository enrollmentRepository;
 
     public Long calculate(Event event) {
-        Optional<Long> countedEvents = enrollmentRepository.countByEventId(event.getId());
-        return event.getMaxCapacity() - countedEvents.orElse(0L);
+
+        long activeEnrollments = enrollmentRepository.countByEventIdAndStatus(
+                event.getId(),
+                EnrollmentStatus.ACTIVE
+        );
+
+        return event.getMaxCapacity() - activeEnrollments;
     }
 }
