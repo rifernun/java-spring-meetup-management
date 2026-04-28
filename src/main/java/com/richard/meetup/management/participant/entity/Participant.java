718efdddd1dc4cad43cdbc29ad7a70931f85a602
Rifernun <rifernun@gmail.com>
2026-04-28 11:41:51 -0300

    feat(participant): add user association to Participant entity

--- src/main/java/com/richard/meetup/management/participant/entity/Participant.java
+++ src/main/java/com/richard/meetup/management/participant/entity/Participant.java
@@ -1,11 +1,13 @@
 package com.richard.meetup.management.participant.entity;
 
 import com.richard.meetup.management.shared.entity.BaseEntity;
+import com.richard.meetup.management.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -24,8 +26,10 @@ public class Participant extends BaseEntity {
     private String name;
 
     @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
     private String linkedin;
+
+    @OneToOne
+    @JoinColumn(name = "id_user")
+    private User user;
+
 }
