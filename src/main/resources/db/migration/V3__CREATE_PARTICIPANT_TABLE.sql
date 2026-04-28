5e8e5799f21e5d3f5ecf758bd409df7d798dd83b
Rifernun <rifernun@gmail.com>
2026-04-28 09:31:12 -0300

    refactor: rename migration files and update tb_participants table schema to include id_user foreign key

--- src/main/resources/db/migration/V3__CREATE_PARTICIPANT_TABLE.sql
+++ src/main/resources/db/migration/V3__CREATE_PARTICIPANT_TABLE.sql
@@ -1,9 +1,10 @@
 CREATE TABLE tb_participants (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
     linkedin VARCHAR(255),
+    id_user UUID NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by VARCHAR(255) NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user) REFERENCES tb_users(id) ON DELETE CASCADE
 );
